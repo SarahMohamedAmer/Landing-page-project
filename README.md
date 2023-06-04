3b628da4ea3d64d93de66326e683ddfcb87e83ea
Sarah Amer <97125596+SarahMohamedAmer@users.noreply.github.com>
2023-06-04 16:34:22 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Landing-page-project
+# Landing Page Project
+
+This project is for Udacity's professional Front-End Development Nanodegree program.
+
+
+## Table of Contents
+
+* Html and css mostly from udacity's startup project with some updates I've made.
+* Using Dom to generate a navbar. 
+* add and remove active class when scrolling through the sections.
+* add a scroll to top button.
+* smooth scroll with css.
+
+ 
+
+## sources i used 
+
+* https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+* In addition to all the sources provided in the Udacity's introductions to the project.
---
+++ js/app.js
@@ -0,0 +1,143 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+ const homeButton = document.getElementById('homeButton');
+ const sectionsElements = document.querySelectorAll('section');
+ const navbarUl = document.getElementById('navbar__list');
+ let navList = '';
+ 
+ /**
+  * End Global Variables
+  * Start Helper Functions
+  
+  *
+  */
+ // Generate navbar from sections id names we got from the querySelectorAll
+ function gernerateNavbar() {
+   sectionsElements.forEach((section) => {
+     // add html tags for list items
+     // dataset.nav returns DOMStringMap {nav: section 1}
+     navList += `<li> <a class="nav__link menu__link" href="#${section.id}" id="navli">
+           ${section.dataset.nav}</a></li>`;
+   });
+   // add the tags to the inner htmls
+   navbarUl.innerHTML = navList;
+ }
+ gernerateNavbar();
+ 
+ // Add class 'active' to section when near top of viewport (Eye level )
+ 
+ function addActiveClass(section) {
+   // get the id from the section
+   const id = section.getAttribute('id');
+ 
+   // add the active class to the section
+   document.querySelector(`#${id}`).classList.add('your-active-class');
+ }
+ 
+ //Removing the active class from the section
+ function removeActiveClass(section) {
+   const id = section.getAttribute('id');
+   document.querySelector(`#${id}`).classList.remove('your-active-class');
+ }
+ 
+ // calcualting when the section is active
+ function makeActiveSection() {
+   sectionsElements.forEach((section) => {
+     // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+     // DOMRect object providing information about the size of an element and its position relative to the viewport.
+     // viewport : A viewport represents a polygonal (normally rectangular) area in computer graphics that is currently being viewed.
+ 
+     let elementOffset = section.getBoundingClientRect();
+     if (elementOffset.top <= 150 && elementOffset.bottom >= 150) {
+       addActiveClass(section);
+     } else {
+       removeActiveClass(section);
+     }
+   });
+ }
+ // event listener to the dom itself so
+ document.addEventListener('scroll', makeActiveSection);
+ 
+ // get a button to scroll back to top of the page
+ //https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function () {
+   scrollFunction();
+ };
+ 
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     homeButton.style.display = 'block';
+   } else {
+     homeButton.style.display = 'none';
+   }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+ }
+ 
+ homeButton.addEventListener('click', topFunction);
+ 
+ let navbar = document.getElementById('navbar').querySelectorAll('li');
+ 
+ // itrate in li items list
+ navbar.forEach((item) => {
+   item.addEventListener('click', function (e) {
+     navbar.forEach((item) => {
+       // remove every navbarclick class added befoe in any list item
+       item.classList.remove('navbarclick');
+     });
+     // add the class on the button
+     item.classList.add('navbarclick');
+   });
+ });
+ 
+ // lis.forEach((li) => {
+ //   li.addEventListener('mouseover', function (e) {
+ //     li.classList.add('navbarclick');
+ //     console.log(li.innerHTML);
+ //   });
+ // });
+ /**
+  * End Helper Functions
+  * Begin Main Functions
+  *
+  */
+ 
+ // build the nav
+ 
+ // Add class 'active' to section when near top of viewport
+ 
+ // Scroll to anchor ID using scrollTO event
+ 
+ /**
+  * End Main Functions
+  * Begin Events
+  *
+  */
+ 
+ // Build menu
+ 
+ // Scroll to section on link click
+ 
+ // Set sections as active
